91abd1a5537e9074a4912de8ab0787629c6b588c
tdrpa <armstrong.wang@gmail.com>
2025-03-10 15:52:59 +0800

    adjust according to tdcore changes

--- demo/en/01 view notepad[about] info/notepad_about.py
+++ demo/en/01 view notepad[about] info/notepad_about.py
@@ -1,10 +1,10 @@
 #import tdcore module
-from tdrpa import tdcore
+from tdrpa.tdcore import LocatorWindows
 
 #get 'Desktop' root element
-desktop=tdcore.LocatorWindows.findElement()
+desktop=LocatorWindows.findElement()
 #print root 'Name' property
-print(desktop._element.Name)
+print(desktop.Name)
 
 #open 'notepad.exe'
 import os
@@ -16,12 +16,12 @@ time.sleep(1)
 
 #click 'Help' menu
 helpSelector="[  { 'wnd' : [ ('Text' , 'Untitled - Notepad') , ('aaRole' , '10') , ('App' , 'notepad.exe') ] } ,  { 'ctrl' : [ ('AutomationId' , 'MenuBar') , ('Text' , 'Application') ] } ,  { 'ctrl' : [ ('Text' , 'Help') , ('aaRole' , '12') ] }]"
-helpElement=tdcore.LocatorWindows.findElement(helpSelector)
-helpElement._element.Click()
+helpElement=LocatorWindows.findElement(helpSelector)
+helpElement.Click()
 
 #click 'About' item
 aboutSelector="[  { 'wnd' : [ ('Text' , 'Untitled - Notepad') , ('aaRole' , '10') , ('App' , 'notepad.exe') ] } ,  { 'wnd' : [ ('Text' , 'Help') , ('aaRole' , '11') ] } ,  { 'ctrl' : [ ('AutomationId' , '65') , ('Text' , 'About Notepad') ] }]"
-aboutElement=tdcore.LocatorWindows.findElement(aboutSelector)
-aboutElement._element.Click()
+aboutElement=LocatorWindows.findElement(aboutSelector)
+aboutElement.Click()
 
-#For the properties and methods of "_element", please refer to the "uiautomation" https://pypi.org/project/uiautomation/ open-source library or use a programming environment with code completion and intelligent suggestions
+#For the properties and methods of "element", please refer to the "uiautomation" https://pypi.org/project/uiautomation/ open-source library or use a programming environment with code completion and intelligent suggestions
--- demo/zh_CN/01 查看notepad【关于】信息/notepad_about.py
+++ demo/zh_CN/01 查看notepad【关于】信息/notepad_about.py
@@ -1,10 +1,10 @@
 #导入tdcore包
-from tdrpa import tdcore
+from tdrpa.tdcore import LocatorWindows
 
 #获取'Desktop'根元素
-desktop=tdcore.LocatorWindows.findElement()
+desktop=LocatorWindows.findElement()
 #输出元素Name属性
-print(desktop._element.Name)
+print(desktop.Name)
 
 #打开notepad.exe
 import os
@@ -16,12 +16,12 @@ time.sleep(1)
 
 #点击'帮助'菜单栏
 helpSelector="[  { 'wnd' : [ ('Text' , '无标题 - 记事本') , ('aaRole' , '10') , ('App' , 'notepad.exe') ] } ,  { 'ctrl' : [ ('AutomationId' , 'MenuBar') , ('Text' , '应用程序') ] } ,  { 'ctrl' : [ ('Text' , '帮助(H)') , ('aaRole' , '12') ] }]"
-helpElement=tdcore.LocatorWindows.findElement(helpSelector)
-helpElement._element.Click()
+helpElement=LocatorWindows.findElement(helpSelector)
+helpElement.Click()
 
 #点击'关于'菜单项
 aboutSelector="[  { 'wnd' : [ ('Text' , '无标题 - 记事本') , ('aaRole' , '10') , ('App' , 'notepad.exe') ] } ,  { 'wnd' : [ ('Text' , '帮助(H)') , ('aaRole' , '11') ] } ,  { 'ctrl' : [ ('AutomationId' , '65') , ('Text' , '关于记事本(A)') ] }]"
-aboutElement=tdcore.LocatorWindows.findElement(aboutSelector)
-aboutElement._element.Click()
+aboutElement=LocatorWindows.findElement(aboutSelector)
+aboutElement.Click()
 
-#"_element"的属性和方法，见"uiautomation" https://pypi.org/project/uiautomation/ 开源库，或使用带代码补全和智能提示的编程环境
+#"element"的属性和方法，见"uiautomation" https://pypi.org/project/uiautomation/ 开源库，或使用带代码补全和智能提示的编程环境
